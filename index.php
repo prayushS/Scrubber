4365fd56cd8b4ac0c546270d3a25c48752ce43ec
prayushS <iprayushs@gmail.com>
2016-08-28 21:37:06 -0400

    initialized repo and

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> scrubber </title>
+	<link rel = "stylesheet" type = "text/css" href = "demo.css"/>
+	<link rel = "stylesheet" type = "text/css" href = "scrubber.css"/>
+</head>
+<body>
+	<a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+	
+	<h1>Scrubber.js</h1>
+	<video class = "video" data-state="play" data-scrub = "off">
+		
+
+		<source src = "http://localhost:8888/videoPlayer/cane.mp4" type = "audio/mp3">
+
+	</video>
+	<!-- 
+			http://localhost:8888/videoPlayer/cane.mp4
+			http://www.html5rocks.com/en/tutorials/video/basics/devstories.webm
+			http://i.imgur.com/zvATqgs.mp4
+
+
+
+			*	File Types
+
+				*	mp3/mp4
+				*	webm
+				*	gif
+				*	gifv
+				
+
+	-->
+	<div class="scrubber-wrapper">
+
+
+  		<div class="scrubber-progress">
+
+			<span class = "scrubber-time hide"></span>
+
+    		<div class="scrubber-seek"></div>
+
+    		<div class="scrubber-seek-shadow hide"></div>
+
+  		</div>
+</div>
+
+
+<div id = "project-description">
+
+	<span id = "note"> <b> ( click the video to play/pause ) </b> </span>
+
+
+	<span class = "log" id = "duration">Duration: </span>
+	<span class = "log" id = "currTime">Current Time: </span>
+	<span class = "log" id = "percentage">Percentage: </span>
+
+	
+
+</div>
+<script src = "scrubber.test.js"></script>
+
+<script>
+	
+	scrubber.initialize();
+</script>
+
+</body>
+</html>
---
+++ scrubber.test.js
@@ -0,0 +1,340 @@
+var scrubber = function(s) {
+
+    var elements = {
+
+            video: document.querySelector('.video'),
+            scrubber: document.querySelector('.scrubber-seek'),
+            scrubberWrapper: document.querySelector('.scrubber-wrapper'),
+            scrubberProgress: document.querySelector('.scrubber-progress'),
+            scrubberShadow: document.querySelector('.scrubber-seek-shadow'),
+            hoverTime: document.querySelector('.scrubber-time')
+
+        },
+        settings = {
+
+            duration: '',
+            _mouseDown: false,
+
+            video: {
+
+                height: 320,
+                width: 640,
+                loop: false,
+                autoplay: false,
+                controls: false,
+                preload: 'metadata',
+                type: 'video/mp4'
+
+            },
+
+            scrubber: {
+
+                showTime: true
+            }
+        },
+
+        logs = {
+
+            duration: document.querySelector('#duration'),
+            percentage: document.querySelector('#percentage'),
+            currTime: document.querySelector('#currTime')
+        }
+
+    /*
+
+
+        *   DONE
+
+            *   perform other checks on mouse events, so the video. doesn't "accedentally" get played
+            *   make the progress on the video. "smoother" ( IMPORTANT )
+            *   need an updateProgress function, which tracks the progress if the video is playing
+        
+        *   TO DO
+
+
+            *   set bounds on the elements.scrubberShadow
+            *   Show Buffer
+
+    */
+
+    extend = function(target, source) {
+
+            for (var key in source) {
+
+                if (source.hasOwnProperty(key)) {
+
+                    target[key] = source[key];
+
+                }
+
+            }
+
+            return target;
+
+        },
+
+        //  given seconds, returns the time in the format
+        //  hh:mm:ss
+        convertTime = function(cSeconds) {
+
+            //  http://stackoverflow.com/a/1322830
+
+            var totalSeconds = Math.trunc(cSeconds),
+                hours = parseInt(totalSeconds / 3600) % 24,
+                minutes = parseInt(totalSeconds / 60) % 60,
+                seconds = totalSeconds % 60;
+
+            return (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+
+
+        },
+
+        findTime = function(mPos) {
+
+            //  given mouse position (mPos) => event.x
+            //  duration of the video and the width of the video bar,
+            //  it returns what the time of the video would be at that point
+
+
+            //  get relative time according to the position of mouse
+            var posToTime = ((mPos - elements.scrubberWrapper.offsetLeft) / elements.scrubberWrapper.offsetWidth) * settings.duration
+
+            return convertTime(posToTime);
+
+
+        },
+
+        hoverTime = function(mousePos, hoverPos) {
+
+
+            //  Shows the time above the progress bar
+
+            //  if true, shows time, else doesn't show
+
+
+            if (settings.scrubber.showTime) {
+
+                elements.hoverTime.innerHTML = findTime(mousePos);
+                elements.hoverTime.classList.remove('hide');
+                elements.hoverTime.setAttribute(
+
+                    'style',
+                    'left: ' + hoverPos + 'px'
+
+                );
+
+            }
+        },
+
+        bind = function() {
+
+
+            //  video. events
+
+            elements.video.addEventListener('loadedmetadata', function() {
+
+                settings.duration = event.target.duration.toFixed(3);
+
+                logs.duration.innerHTML = "Duration: " + settings.duration + " seconds";
+
+            }, false);
+
+            elements.video.addEventListener('click', function() {
+
+
+                console.log(this.getAttribute('data-state'));
+
+                switch (this.getAttribute('data-state')) {
+
+
+                    case 'play':
+
+
+                        this.play();
+
+                        this.setAttribute('data-state', 'pause');
+
+                        break;
+
+
+
+                    case 'rewind':
+
+                        this.play(0);
+
+                        this.setAttribute('data-state', 'pause');
+
+                        break;
+
+
+                    case 'pause':
+
+                        this.pause();
+
+                        this.setAttribute('data-state', 'play');
+
+                        break;
+
+                }
+
+
+            }, false);
+
+
+            elements.video.addEventListener('timeupdate', function() {
+                updateProgress();
+            }, false);
+
+            elements.video.addEventListener('ended', function() {
+                this.setAttribute('data-state', 'rewind');
+
+            }, false);
+
+
+            elements.scrubberWrapper.addEventListener('mousedown', mouseDown, false);
+
+            elements.scrubberWrapper.addEventListener('mousemove', mouseMove, false);
+
+            elements.scrubberWrapper.addEventListener('mouseup', mouseUp, false);
+
+            elements.scrubberWrapper.addEventListener('mouseout', mouseOut, false);
+
+        },
+
+
+        updateProgress = function() {
+
+
+            //update the progress bar
+
+            elements.scrubberProgress.setAttribute(
+
+                'style',
+
+                'width: ' + ((100 / settings.duration) * elements.video.currentTime) + '%'
+
+            );
+
+            logs.currTime.innerHTML = "Current Time: " + elements.video.currentTime + " seconds";
+
+            logs.percentage.innerHTML = "Percentage: " + ((100 / settings.duration) * elements.video.currentTime) + '%';
+
+            console.log('running');
+        },
+
+        //events
+        mouseDown = function() {
+            settings._mouseDown = true;
+            elements.scrubber.classList.add('scrubber-seek-clicked');
+            elements.video.pause();
+            elements.video.setAttribute('data-state', 'play');
+            elements.video.setAttribute('data-scrub', 'on');
+
+            elements.video.currentTime = ((event.x - this.offsetLeft) / this.offsetWidth) * settings.duration;
+
+        },
+        mouseOut = function() {
+            settings._mouseDown = false;
+
+
+            if (settings.scrubber.showTime) {
+
+                elements.hoverTime.classList.add('hide');
+
+            }
+
+            if (elements.video.getAttribute('data-scrub') == "on") {
+
+                elements.video.play();
+
+            }
+            elements.video.setAttribute('data-scrub', 'off');
+            elements.scrubberShadow.classList.add('hide');
+
+        },
+        mouseMove = function() {
+            //  set the scrubber equal to the mouse position
+
+            //  check if the mousedown variable is true
+
+            var scrubberPos = ((event.x - this.offsetLeft) - elements.scrubber.offsetWidth / 2);
+
+            elements.scrubberShadow.classList.remove('hide');
+
+            elements.scrubberShadow.setAttribute(
+
+                'style',
+                'left: ' + scrubberPos + 'px'
+
+            )
+            hoverTime(
+
+                event.x,
+                scrubberPos
+
+            );
+
+            if (settings._mouseDown) {
+
+                elements.video.currentTime = ((event.x - this.offsetLeft) / this.offsetWidth) * settings.duration;
+
+
+            }
+
+
+
+
+        },
+        mouseUp = function() {
+            settings._mouseDown = false;
+            elements.scrubber.classList.remove('scrubber-seek-clicked');
+            if (settings.scrubber.showTime) {
+
+                elements.hoverTime.classList.add('hide');
+
+            }
+
+            elements.video.play();
+            elements.video.setAttribute('data-state', 'pause');
+            elements.video.setAttribute('data-scrub', 'off');
+
+            elements.video.currentTime = ((event.x - this.offsetLeft) / this.offsetWidth) * settings.duration;
+
+
+
+
+        };
+
+
+    s = {
+
+        initialize: function(videoOptions, scrubberOptions) {
+
+            extend(settings.video, videoOptions);
+
+            extend(settings.scrubber, scrubberOptions);
+
+            for (var key in settings.video) {
+
+
+                elements.video[key] = settings.video[key];
+
+            }
+
+            bind();
+
+        },
+
+        getDuration: function() {
+
+            return settings.duration;
+
+        }
+
+
+    };
+
+
+
+    return s;
+}(scrubber);
